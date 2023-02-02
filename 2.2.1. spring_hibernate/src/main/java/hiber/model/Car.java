adfdc62f6f20258440d0d41d6b981192b9b759e4
Wiack <viiakko@gmail.com>
2023-02-02 10:26:57 +0300

    1

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -0,0 +1,65 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String model;
+    @Column
+    private int series;
+
+    @OneToOne(mappedBy = "car")
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Car(String model, int series) {
+
+        this.model = model;
+        this.series = series;
+    }
+
+    public Car() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    @Override
+    public String toString() {
+        return "Car: " + model + ", " + series;
+    }
+}
